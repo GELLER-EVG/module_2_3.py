4ac2625c357275ffbf4f8b3b14e80d44fd913b16
evgenia kolmakova <evgeniageller000@gmail.com>
2024-08-14 16:18:09 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -2,8 +2,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while  len(my_list) > a:
-        if my_list[a] > 0:
-            print(my_list[a])
+        if my_list[a] < 0:
+            break
+        elif my_list[a] ==0:
+            a += 1
+            continue
+
+        print(my_list[a])
         a += 1 
 
 
